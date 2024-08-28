e6cd2a061ba547a1d4fd7a19007c9b07ac168d85
Dawid Cedrych <dawidcedrych@Dawids-MacBook-Pro.local>
2024-08-28 13:38:09 +0200

    added files

---
+++ pages/fonts.js
@@ -0,0 +1,12 @@
+import { Inter, Roboto_Mono } from 'next/font/google'
+import localFont from 'next/font/local';
+ 
+
+ 
+export const roboto_mono = Roboto_Mono({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: "--font-test",
+  })
+
+export const suis = localFont({ src: '../public/fonts/SuisseIntl-Regular-WebM.woff2' })
